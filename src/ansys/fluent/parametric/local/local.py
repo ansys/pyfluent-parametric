e9a9fdaf05b1c34f9ee8023c9ea8a8568d9299d1
ANSYS\spearson <sean.pearson@ansys.com>
2022-07-19 19:40:35 +0100

    nice refactor

--- src/ansys/fluent/parametric/local/local.py
+++ src/ansys/fluent/parametric/local/local.py
@@ -1,53 +1,7 @@
 """
 Classes for running a parametric study in Fluent.
 
-Example
--------
-from ansys.fluent.addons.parametric import ParametricStudy, DesignPointStatus  # noqa: E501
 
-Instantiate the study, specifying the case
-
-study = ParametricStudy(case_file_name=my_case_file_name)
-
-Add one new design point and set an input parameter
-
-dp1 = study.add_design_point("DP1")
-dp1.set_input("parameter_1", 0.235)
-dp1.set_input("velocity_inlet_5_y_velocity", 0.772)
-
-The solver has not been run yet so no outputs are computed
-The design point is out of date
-
-assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
-
-Get the base design point
-
-base = study.design_point("Base DP")
-
-Check that block updates works
-
-dp1.block_updates()
-base.block_updates()
-assert(dp1.status == DesignPointStatus.BLOCKED)
-study.update_all()
-assert(dp1.outputs == base.outputs)
-
-Update the design points
-
-dp1.allow_updates()
-base.allow_updates()
-assert(dp1.status == DesignPointStatus.OUT_OF_DATE)
-study.update_all()
-
-Check that dp1 is up to date and that the outputs differ
-
-assert(dp1.status == DesignPointStatus.UPDATED)
-assert(dp1.outputs != base.outputs)
-
-It's important to clean up any studies to help ensure that
-all Fluent sessions are cleaned up
-
-del study
 """
 
 from enum import Enum
@@ -58,114 +12,57 @@ from ansys.fluent.core.utils.async_execution import asynchronous
 
 from .filereader.casereader import CaseReader
 
-from ansys.fluent.parametric import ParametricSession as FluentParametricSession
+from ansys.fluent.parametric import ParametricSession
 from ansys.fluent.parametric import BASE_DP_NAME
 
 
-class DesignPointStatus(Enum):
+class LocalDesignPoint:
     """
-    Status of a design point in a parametric study.
-
-    Attributes
-    ----------
-    OUT_OF_DATE : int
-    UPDATING : int
-    UPDATED : int
-    FAILED : int
-    BLOCKED : int
-    """
-
-    OUT_OF_DATE = 1
-    UPDATING = 2
-    UPDATED = 3
-    FAILED = 4
-    BLOCKED = 5
-
-
-class DesignPoint:
-    """
-    Design point in a parametric study.
+    Purely local version of design point in a parametric study.
 
     Attributes
     ----------
     name : str
         Name of the design point as a str.
-    outputs : dict
+    output_parameters : dict
         Dict of output parameters
         (name of parameter to value).
-    inputs : dict
+    input_parameters : dict
         Dict of input parameters
         (name of parameter to value).
     status : DesignPointStatus
         Current status of the design point.
-
-    Methods
-    -------
-    set_input(parameter_name: str, value)
-        Set one parameter in the design point to the value provided.
-    on_end_updating(outputs: dict)
-        Inform the design point that it is in an UPDATED state and
-        provides the associated output parameters.
-    block_updates()
-        Move the design point into a do not update state.
-    block_updates()
-        Move the design point into a needs update state.
     """
 
     def __init__(self, design_point_name: str, base_design_point=None):
         self.name = design_point_name
         if base_design_point:
-            self.__inputs = base_design_point.inputs.copy()
-            self.__outputs = base_design_point.outputs.copy()
+            self.__inputs = base_design_point.input_parameters.copy()
+            self.__outputs = base_design_point.output_parameters.copy()
         else:
             self.__inputs = {}
             self.__outputs = {}
-        # TODO add listener for __status:
-        self.__status = DesignPointStatus.OUT_OF_DATE
 
     @property
-    def inputs(self) -> dict:
+    def input_parameters(self) -> dict:
         return self.__inputs
 
-    @inputs.setter
-    def inputs(self, inputs: dict):
-        self.__status = DesignPointStatus.OUT_OF_DATE
+    @input_parameters.setter
+    def input_parameters(self, inputs: dict):
         self.__inputs = inputs
 
     @property
-    def outputs(self) -> dict:
+    def output_parameters(self) -> dict:
         return self.__outputs
 
-    @outputs.setter
-    def outputs(self, outputs: dict):
-        self.__status = DesignPointStatus.OUT_OF_DATE
+    @output_parameters.setter
+    def output_parameters(self, outputs: dict):
         self.__outputs = outputs
 
-    @property
-    def status(self) -> DesignPointStatus:
-        return self.__status
 
-    def set_input(self, parameter_name: str, value):
-        self.__status = DesignPointStatus.OUT_OF_DATE
-        self.__inputs[parameter_name] = value
-
-    def on_start_updating(self):
-        self.__status = DesignPointStatus.UPDATING
-
-    def on_end_updating(self, outputs: dict):
-        self.__outputs = outputs
-        self.__status = DesignPointStatus.UPDATED
-
-    def block_updates(self):
-        self.__status = DesignPointStatus.BLOCKED
-
-    def allow_updates(self):
-        self.__status = DesignPointStatus.OUT_OF_DATE
-
-
-class DesignPointTable(list):
+class LocalDesignPointTable(list):
     """
-    Design point study in a parametric study
+    Local version of Design point table in a parametric study
 
     Methods
     -------
@@ -187,15 +84,15 @@ class DesignPointTable(list):
             If the design point is not found.
     """
 
-    def __init__(self, base_design_point: DesignPoint):
+    def __init__(self, base_design_point: LocalDesignPoint):
         super().__init__()
         self.append(base_design_point)
 
-    def add_design_point(self, design_point_name: str) -> DesignPoint:
-        self.append(DesignPoint(design_point_name, self[0]))
+    def add_design_point(self, design_point_name: str) -> LocalDesignPoint:
+        self.append(LocalDesignPoint(design_point_name, self[0]))
         return self[-1]
 
-    def find_design_point(self, idx_or_name) -> DesignPoint:
+    def find_design_point(self, idx_or_name) -> LocalDesignPoint:
         if isinstance(idx_or_name, int):
             return self[idx_or_name]
         for design_point in self:
@@ -210,191 +107,14 @@ class DesignPointTable(list):
         self.remove(self.find_design_point(idx_or_name))
 
 
-class FluentParameterAccessor:
+class LocalParametricStudy:
     """
-    Extracts parameter name to value dicts from table strs
-    currently returned by the API
-
-    Attributes
-    ----------
-    input_parameters : dict
-        The current input parameter dict.
-    output_parameters : dict
-        The current input parameter dict.
-    """
-
-    def __init__(self, fluent_session):
-        self.__list_parameters = (
-            fluent_session.tui.solver.define.parameters.list_parameters
-        )
-
-    @property
-    def input_parameters(self) -> dict:
-        return FluentParameterAccessor.__parameter_table_to_dict(
-            self.__list_parameters.input_parameters()
-        )
-
-    @property
-    def output_parameters(self) -> dict:
-        return FluentParameterAccessor.__parameter_table_to_dict(
-            self.__list_parameters.output_parameters()
-        )
-
-    @staticmethod
-    def __parameter_table_to_dict(table: str) -> dict:
-        # this code has become more complex now. Originally table was
-        # str here - now ExecuteCommandResult is returned by the calls
-        # to (in|out)put_parameters()
-        table_str = table
-        if not isinstance(table, str):
-            try:
-                table_str = table.result
-            except AttributeError as attr_err:
-                raise RuntimeError(
-                    "Unexpected design point table "
-                    f"type in parse: {type(table)}"
-                ) from attr_err
-        data_lines = table_str.splitlines()[3:]
-        table_as_dict = {}
-        for line in data_lines:
-            line_as_list = line.split()
-            table_as_dict[line_as_list[0]] = line_as_list[1]
-        return table_as_dict
-
-
-class ParametricSession:
-    """
-    Full set of interactions with Fluent in the context of a parametric study
-
-    Attributes
-    ----------
-    input_parameters : dict
-        The current input parameter dict.
-    output_parameters : dict
-        The current input parameter dict.
-
-    Methods
-    -------
-    set_input_parameter(parameter_name: str)
-        Set a single input parameter value in the Fluent session.
-    initialize_with_case(case_file_name: str)
-        Read the specified case into Fluent.
-    update()
-        Run the solver until convergence.
-    """
-
-    def __init__(self, fluent_session):
-        self.__fluent_session = fluent_session
-        self.__parameter_accessor = FluentParameterAccessor(fluent_session)
-
-    @property
-    def input_parameters(self) -> dict:
-        return self.__parameter_accessor.input_parameters
-
-    @property
-    def output_parameters(self) -> dict:
-        return self.__parameter_accessor.output_parameters
-
-    def set_input_parameter(self, parameter_name: str, value):
-        self.__fluent_session.tui.solver.define.parameters.input_parameters.edit(  # noqa: E501
-            parameter_name, parameter_name, value
-        )
-
-    def initialize_with_case(self, case_file_name: str):
-        self.__fluent_session.tui.solver.file.read_case(
-            case_file_name=case_file_name
-        )
-
-    def update(self):
-        self.__fluent_session.tui.solver.solve.initialize.initialize_flow()
-        self.__fluent_session.tui.solver.solve.iterate()
-
-    def __del__(self):
-        self.__fluent_session.exit()
-
-
-class FluentLauncher:
-    """
-    Launches fluent sessions.
-
-    Methods
-    -------
-    __call__()
-        Launch a session
-    """
-
-    def __call__(self):
-        return ParametricSession(fluent_session=pyfluent.launch_fluent())
-
-
-class ParametricStudy:
-    """
-    Parametric study that manages design points to parametrize a
-    Fluent solver set-up. Provides ability to run Fluent for a series
-    of design points, and access the inputs and outputs.
-
-    Methods
-    -------
-    update_all()
-        Bring all design point outputs up to date by running the
-        solver on each design point. Ignores BLOCKED design points.
-    update_design_point()
-        Bring the outputs of the specified design point up to date
-        by running the solver.
-    add_design_point(design_point_name: str) -> DesignPoint
-        Add a design point
-    design_point(idx_or_name)
-        Get a design point, either by name (str) or an index
-        indicating the position in the table (by order of insertion).
-        Raises
-        ------
-        RuntimeError
-            If the design point is not found.
-    """
-
-    def __init__(
-        self,
-        case_file_name: str = "",
-        base_design_point_name: str = "Base DP",
-        launcher=None,
-    ):
-        self.__session = launcher() if launcher else FluentLauncher()
-        if case_file_name:
-            self.__session.initialize_with_case(case_file_name)
-        base_design_point = DesignPoint(base_design_point_name)
-        base_design_point.inputs = self.__session.input_parameters.copy()
-        base_design_point.outputs = self.__session.output_parameters.copy()
-        self.design_point_table = DesignPointTable(base_design_point)
-
-    def update_all(self):
-        for design_point in self.design_point_table:
-            if design_point.status != DesignPointStatus.BLOCKED:
-                self.update_design_point(design_point)
-
-    def update_design_point(self, design_point: DesignPoint):
-        design_point.on_start_updating()
-        for parameter_name, value in design_point.inputs.items():
-            self.__session.set_input_parameter(parameter_name, value)
-        self.__session.update()
-        design_point.on_end_updating(
-            outputs=self.__session.output_parameters.copy()
-        )
-
-    def add_design_point(self, design_point_name: str) -> DesignPoint:
-        return self.design_point_table.add_design_point(design_point_name)
-
-    def design_point(self, idx_or_name) -> DesignPoint:
-        return self.design_point_table.find_design_point(idx_or_name)
-
-
-class CaseParametricStudy:
-    """
-    Parametric study that manages design points to parametrize a
+    Local version of parametric study that manages design points to parametrize a
     Fluent solver set-up.
 
     Methods
     -------
-    add_design_point(design_point_name: str) -> DesignPoint
+    add_design_point(design_point_name: str) -> LocalDesignPoint
         Add a design point
     design_point(idx_or_name)
         Get a design point, either by name (str) or an index
@@ -411,7 +131,7 @@ class CaseParametricStudy:
         base_design_point_name: str = "Base DP"
     ):
         self.case_filepath = case_filepath
-        base_design_point = DesignPoint(base_design_point_name)
+        base_design_point = LocalDesignPoint(base_design_point_name)
         case_reader = CaseReader(case_file_path=case_filepath)
 
         def parameter_info(parameter):
@@ -429,18 +149,18 @@ class CaseParametricStudy:
 
         set_parameter_info(
             source=case_reader.input_parameters(),
-            target=base_design_point.inputs)
+            target=base_design_point.input_parameters)
 
         set_parameter_info(
             source=case_reader.output_parameters(),
-            target=base_design_point.outputs)
+            target=base_design_point.output_parameters)
         
-        self.design_point_table = DesignPointTable(base_design_point)
+        self.design_point_table = LocalDesignPointTable(base_design_point)
 
-    def add_design_point(self, design_point_name: str) -> DesignPoint:
+    def add_design_point(self, design_point_name: str) -> LocalDesignPoint:
         return self.design_point_table.add_design_point(design_point_name)
 
-    def design_point(self, idx_or_name) -> DesignPoint:
+    def design_point(self, idx_or_name) -> LocalDesignPoint:
         return self.design_point_table.find_design_point(idx_or_name)
 
 
@@ -457,7 +177,7 @@ def run_local_study_in_fluent(
         study_input = []
         for idx in design_point_range:
             design_point = local_study.design_point(idx_or_name = idx)
-            study_input.append(design_point.inputs.copy())
+            study_input.append(design_point.input_parameters.copy())
         return study_input
 
     def make_input_for_studies(num_servers) -> None:
@@ -475,7 +195,7 @@ def run_local_study_in_fluent(
 
     @asynchronous
     def make_parametric_session(case_filepath):
-        return FluentParametricSession(case_filepath=case_filepath)
+        return ParametricSession(case_filepath=case_filepath)
 
     @asynchronous
     def apply_to_study(study, inputs):
@@ -530,7 +250,7 @@ def run_local_study_in_fluent(
 
     for study in studies:
         for _, design_point in study.design_points.items():
-            next(it).outputs = design_point.output_parameters.copy()
+            next(it).output_parameters = design_point.output_parameters.copy()
 
 
 
