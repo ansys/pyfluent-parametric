c42b5b41fe3480b8902bd23cf1bd06467a8c4f6b
ANSYS\spearson <sean.pearson@ansys.com>
2022-07-20 22:57:38 +0100

    h5py

--- tests/test_parametric_local.py
+++ tests/test_parametric_local.py
@@ -19,11 +19,24 @@ def test_parametric_local():
         precision="double", processor_count=2, start_transcript=False
     )
 
+    ############################################################################
+    # Read the hopper/mixer case
+
     import_filename = examples.download_file(
         "Static_Mixer_main.cas.h5", "pyfluent/static_mixer"
     )
 
     session.solver.tui.file.read_case(case_file_name=import_filename)
+
+    ############################################################################
+    # Set number of iterations to 100
+    session.solver.tui.solve.set.number_of_iterations("100")
+
+    ############################################################################
+    # Create input parameters after enabling parameter creation in the TUI:
+    # Parameter values:
+    # Inlet1: velocity (inlet1_vel) 0.5 m/s and temperature (inlet1_temp) at 300 K
+    # Inlet2: velocity (inlet2_vel) 0.5 m/s and temperature (inlet2_temp) at 350 K
 
     session.solver.tui.define.parameters.enable_in_TUI("yes")
 
@@ -40,7 +53,10 @@ def test_parametric_local():
         "inlet2", (), "temperature", "yes", "no", "inlet2_temp", 350, "quit"
     )
 
-    session.solver.root.solution.report_definitions.surface.create("outlet-temp-avg")
+    ###########################################################################
+    # Create output parameters using report definitions
+
+    session.solver.root.solution.report_definitions.surface["outlet-temp-avg"] = {}
     session.solver.root.solution.report_definitions.surface[
         "outlet-temp-avg"
     ].report_type = "surface-areaavg"
@@ -51,7 +67,7 @@ def test_parametric_local():
         "outlet-temp-avg"
     ].surface_names = ["outlet"]
 
-    session.solver.root.solution.report_definitions.surface.create("outlet-vel-avg")
+    session.solver.root.solution.report_definitions.surface["outlet-vel-avg"] = {}
     session.solver.root.solution.report_definitions.surface[
         "outlet-vel-avg"
     ].report_type = "surface-areaavg"
@@ -70,12 +86,17 @@ def test_parametric_local():
         "report-definition", "outlet-vel-avg"
     )
 
-    param_case_path = str(
-        Path(pyfluent.EXAMPLES_PATH) / "Static_Mixer_Parameters.cas.h5"
-    )
-    session.solver.tui.file.write_case(param_case_path)
+    ###########################################################################
+    # Enable convergence condition check
 
-    local_study = LocalParametricStudy(case_filepath=param_case_path)
+    session.solver.tui.solve.monitors.residual.criterion_type("0")
+
+    ###########################################################################
+    # Write case with all the settings in place
+    case_path = str(Path(pyfluent.EXAMPLES_PATH) / "Static_Mixer_Parameters.cas.h5")
+    session.solver.tui.file.write_case(case_path)
+
+    local_study = LocalParametricStudy(case_filepath=case_path)
 
     base_design_point = local_study.design_point("Base DP")
 
