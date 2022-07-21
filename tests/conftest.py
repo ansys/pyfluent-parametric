f787c0dea6f509b607493c60faa345b62110d9f6
Anurag Kulshrestha <anurag.kulshrestha@ansys.com>
2022-07-22 01:50:49 +0530

    Fixed existing test

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+pytest_plugins = [
+    "util.fixture_fluent",
+]
+
+@pytest.fixture
+def with_launching_container(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("PYFLUENT_LAUNCH_CONTAINER", "1")
