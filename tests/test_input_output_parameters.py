5ed09305231db669bb1950d7536747ba8978b1e0
Prithwish Mukherjee <prithwish.mukherjee@ansys.com>
2022-08-26 10:05:19 +0530

    Updated code w.r.t. new launch modes

--- tests/test_input_output_parameters.py
+++ tests/test_input_output_parameters.py
@@ -5,9 +5,9 @@ from ansys.fluent.parametric.parameters import InputParameters, OutputParameters
 
 def test_input_output_parameters(new_session):
     case_filepath = examples.download_file("nozzle-2D-WB.cas.h5", "pyfluent/optislang")
-    session = new_session
-    session.solver.root.file.read(file_name=case_filepath, file_type="case")
-    inp = InputParameters(session)
+    solver_session = new_session
+    solver_session.file.read(file_name=case_filepath, file_type="case")
+    inp = InputParameters(solver_session)
     assert len(inp) == 1
     assert inp["inlet_pressure"] == "0.9 [atm]"
     assert inp.get_unit_label("inlet_pressure") == "atm"
@@ -28,6 +28,6 @@ def test_input_output_parameters(new_session):
     assert inp["inlet_pressure"] == "90001 [Pa]"
     assert inp.get_unit_label("inlet_pressure") == "Pa"
 
-    outp = OutputParameters(session)
+    outp = OutputParameters(solver_session)
     assert len(outp) == 1
     assert outp["mass-outlet-op"] == "0.0 [kg/s]"
