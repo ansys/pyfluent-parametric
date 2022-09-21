65a9d1098c91aa2b85c4c2a426cf2c0e1ed17124
Harshal Pohekar <harshal.pohekar@ansys.com>
2022-09-21 15:20:12 +0530

    Added vesion_info() to parametric

--- src/ansys/fluent/parametric/__init__.py
+++ src/ansys/fluent/parametric/__init__.py
@@ -64,7 +64,24 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata
 
+_VERSION_INFO = None
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
+
+
+def version_info() -> str:
+    """Method returning the version of PyFluent being used.
+
+    Returns
+    -------
+    str
+        The PyFluent version being used.
+
+    Notes
+    -------
+    Only available in packaged versions. Otherwise it will return __version__.
+    """
+    return _VERSION_INFO if _VERSION_INFO is not None else __version__
+
 
 BASE_DP_NAME = "Base DP"
 
